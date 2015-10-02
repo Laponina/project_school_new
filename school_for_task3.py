823952e5ef26d9db17a05afe8b2210dd90a45cd8
laponina <laponinalubagmail.com>
2015-10-02 18:58:22 +0700

    все норм

--- school_for_task3.py
+++ school_for_task3.py
@@ -1,19 +1,18 @@
-
 import json
 
 f = open('Students_prob.json', 'r+', encoding='UTF-8')
 students = json.load(f)
+f.close()
 new_student = {
-            "name": "Василий",
-            "middle_name": "Петрович",
-            "surname": "Красный",
-            "school": "12 гимназия",
-            "class": "7 А",
-            "birth_day": "01.12.1996"
-            }
+    "name": "Василий",
+    "middle_name": "Петрович",
+    "surname": "Красный",
+    "school": "12 гимназия",
+    "class": "7 А",
+    "birth_day": "01.12.1996"
+}
 students.append(new_student)
-for el in students:
-    f.write(json.dumps(el['name'], ensure_ascii=False))
-
+f = open('Students_prob.json', 'w', encoding='UTF-8')
+f.write(json.dumps(students, ensure_ascii=False) + '\n')
 
 f.close()
